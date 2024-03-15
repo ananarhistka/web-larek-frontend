2afbc119b70b18f7e45c6270064b54b5d18f51bb
Alex <tyumentseva.shura@yandex.ru>
2024-03-15 22:34:20 +0300

    добавление интерфейсов

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+type LotSection = "софт-скил" | "другое" | "дополнительное" | "кнопка" | "хард-скил";
+type PaymentMethod = "онлайн" | "при получении"
+
+export interface Product {
+  products: CartProduct[];
+  category: LotSection;
+}
+
+export interface AddProduct {
+  addToCart: ()=> void;
+  removeFromeCars: ()=> void;
+}
+
+export interface CartProduct { 
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+export interface Cart {
+  products: CartProduct[];
+  totalPrice: number;
+}
+
+type ProductItem = Product & AddProduct;
+
+export interface DeliveryAddress {
+  adress: string | number;
+  payment: PaymentMethod;
+}
+
+export interface MakingAnOrder {
+  amail: string;
+  phone: string;
+}
