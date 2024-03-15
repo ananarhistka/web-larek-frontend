c46a65bce922b8ccbd484554cd24b4e38c8b0094
Alex <tyumentseva.shura@yandex.ru>
2024-03-15 16:34:37 +0300

    документация

---
+++ src/components/base/Component.ts
@@ -0,0 +1,55 @@
+import { IEvents } from './events';
+
+export abstract class Component<T> {
+
+  protected constructor(
+    protected readonly container: HTMLElement,
+    protected events: IEvents) {
+  }
+
+  // Переключить класс
+  toggleClass(element: HTMLElement, className: string, force?: boolean) {
+    element.classList.toggle(className, force);
+  }
+
+  // Установить текстовое содержимое
+  protected setText(element: HTMLElement, value: unknown): void {
+    if (element) {
+      element.textContent = String(value);
+    }
+  }
+
+  // Сменить статус блокировки
+  setDisabled(element: HTMLElement, state: boolean): void {
+    if (element) {
+      if (state) element.setAttribute('disabled', 'disabled');
+      else element.removeAttribute('disabled');
+    }
+  }
+
+  // Скрыть элемент
+  protected setHidden(element: HTMLElement) {
+    element.style.display = 'none';
+  }
+
+  // Показать элемент
+  protected setVisible(element: HTMLElement) {
+    element.style.removeProperty('display');
+  }
+
+  // Установить изображение с алтернативным текстом
+  protected setImage(element: HTMLImageElement, src: string, alt?: string) {
+    if (element) {
+      element.src = src;
+      if (alt) {
+        element.alt = alt;
+      }
+    }
+  }
+
+  // Вернуть корневой DOM-элемент
+  render(data?: Partial<T>): HTMLElement {
+    Object.assign(this as object, data ?? {});
+    return this.container;
+  }
+}
